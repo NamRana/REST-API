7f4f112c40e17a01d8755c74d484276a45111015
Naman <sanjeevnamanrana@gmail.com>
2022-10-01 12:07:09 +0530

    Learning basics of rest api

---
+++ controllers/goalController.js
@@ -0,0 +1,54 @@
+const asyncHandeler=require('express-async-handler');
+const Goal=require('../models/goalModel')
+
+const getGoals=asyncHandeler(async (req,res)=>{
+    const goals=await Goal.find()
+    res.status(200).json(goals);
+
+})
+
+const setGoals=asyncHandeler(async (req,res)=>{
+    if(!req.body.text){
+        res.status(404)
+        throw new Error('Please add a text');
+    }
+
+    const goal=await Goal.create({
+        text:req.body.text
+    })
+    res.status(200).json(goal);
+})
+
+
+const updateGoals=asyncHandeler(async (req,res)=>{
+
+    const goal=await Goal.findById(req.params.id);
+    if(!goal){
+        res.status(400);
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal=await Goal.findByIdAndUpdate(req.params.id,req.body,{new :true});
+    res.status(200).json(updatedGoal);
+})
+
+
+const deleteGoals=asyncHandeler(async (req,res)=>{
+
+    const goal=await Goal.findById(req.params.id);
+    if(!goal){
+        res.status(400);
+        throw new Error('Goal not found')
+    }
+
+    goal.remove();
+    res.status(200).json({id:req.params.id});
+})
+
+
+module.exports={
+    getGoals,
+    setGoals,
+    updateGoals,
+    deleteGoals
+}
